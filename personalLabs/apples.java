52f1396707ad1ac85bf64f2e9cfca274c94fd0b8
Victor Delaplaine <vdelaplaines@aol.com>
2018-11-14 21:36:04 -0800

    Create apples.java

---
+++ personalLabs/apples.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+public class apples{
+
+
+
+        public static void main(String [] args){
+                createfile g = new createfile();
+                g.openFile();
+                g.addRecords();
+                g.closeFile();
+
+
+        }
+
+}
